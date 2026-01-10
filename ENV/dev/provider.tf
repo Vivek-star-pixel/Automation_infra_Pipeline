70fef70062009d4a91e592db597607c1cd5a9ab4
Vivek <vivekmohkar170@gmail.com>
2026-01-10 19:38:41 +0530

    Update storage account name in provider.tf

--- ENV/dev/provider.tf
+++ ENV/dev/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.5.0" #updated batao
   backend "azurerm" {
     resource_group_name  = "vky"
-    storage_account_name = "keystroke11111"
+    storage_account_name = "keystroke111111"
     container_name       = "mysecrettffile"
     key                  = "secretfile1.tfstate"
     subscription_id      = "a0366c1d-7e23-4176-93a9-0e084725b2d4"
