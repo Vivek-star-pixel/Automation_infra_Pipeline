a032044d9125534739912083e638865b55b101d0
Vivek <vivekmohkar170@gmail.com>
2026-01-10 19:43:09 +0530

    Update provider.tf

--- ENV/dev/provider.tf
+++ ENV/dev/provider.tf
@@ -5,7 +5,7 @@ terraform {
     storage_account_name = "keystroke111111"
     container_name       = "mysecrettffile"
     key                  = "secretfile1.tfstate"
-    subscription_id      = "a0366c1d-7e23-4176-93a9-0e084725b2d4"
+    subscription_id      = "7e450572-1056-4117-9e7b-97e717138408"
   }
   required_providers {
     azurerm = {
@@ -17,5 +17,5 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  subscription_id = "a0366c1d-7e23-4176-93a9-0e084725b2d4"
+  subscription_id = "7e450572-1056-4117-9e7b-97e717138408"
 }
